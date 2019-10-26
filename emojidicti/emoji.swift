1c3449e1ff261d6a0dd2375cd0e37e190aea73b1
AcheZeta <ache.mariana@gmail.com>
2019-10-26 13:23:57 -0500

    First commit

---
+++ emojidicti/emoji.swift
@@ -0,0 +1,16 @@
+//
+//  emoji.swift
+//  emojidicti
+//
+//  Created by Laboratoria on 10/26/19.
+//  Copyright © 2019 equipo5. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
